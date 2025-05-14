2dca84193f0e8dd4a8ebd9cf05e037c1ae1e34bf
Arthur <Arthur.fabricyo@gmail.com>
2025-05-13 23:47:50 -0300

    appTotem

--- App.js
+++ App.js
@@ -2,19 +2,30 @@ import React from "react"
 import {View, Text, Image} from "react-native"
 
 function App(){
-  let foto = "https://static.vecteezy.com/system/resources/previews/022/600/817/large_2x/english-letter-a-for-kids-3d-letter-capital-a-small-a-free-vector.jpg"
   let nameCaba = "Arthur"
   return(
     <View style= {{padding: 20}}>
       <Text>${nameCaba}</Text>
       <Text style= {{color: "#F00FFF", fontSize: 35, fontWeight: 30}}>Meu primero App</Text>
-      <Image
-      source={{uri: foto}}
-      style={{width: 200, height: 200}}
-      ></Image>
-
       
+  <Logo Largura ={100}  Altura ={100} Name ={"Fabrica do gás"}/>
+
     </View>
   )
 }
 export default App
+
+function Logo(propos){
+  let img = "https://static.vecteezy.com/system/resources/previews/022/600/817/large_2x/english-letter-a-for-kids-3d-letter-capital-a-small-a-free-vector.jpg"
+  return(
+    <View>
+    <Image source={{ uri: img }} style= {{ width: propos.Largura, height: propos.Altura }}>
+      </Image>
+      <Text>
+        {propos.Name}
+      </Text>
+      </View>
+
+    
+  )
+}
