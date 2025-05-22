9763e379c9eec299c9d3ffb7be25e752540fd764
Arthur <Arthur.fabricyo@gmail.com>
2025-05-22 20:05:04 -0300

    melhorias definitivas

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import AppNavigator from './src/navegation/appNavegator';
 
-export default function App() {
-
- /* useEffect(() => {
-    navigate("Home"); // Ao gerar o app, o apk lança o usuário para essa tela em branca. Remova esses comentários antes de gerar o biuld.
-  }, []);*/
-
+export default function App( {navigation} ) {
+/*
+  useEffect(() => {
+    navigation.navigate("Home"); // Ao gerar o app, o apk lança o usuário para essa tela em branca. Remova esses comentários antes de gerar o biuld.
+  }, []);
+*/
   return <AppNavigator />;
 }
